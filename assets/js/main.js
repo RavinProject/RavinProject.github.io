17bb17258bc237676e4b52414d5cf70825e78ac9
Regis Amaral <amaral.regis@gmail.com>
2023-07-19 19:07:03 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,22 +81,22 @@ function loaderActions(){
      });
     }
 
-    // projects filters isotop
-    $(".product-filters li").on('click', function () {
+    // // projects filters isotop
+    // $(".product-filters li").on('click', function () {
         
-        $(".product-filters li").removeClass("active");
-        $(this).addClass("active");
+    //     $(".product-filters li").removeClass("active");
+    //     $(this).addClass("active");
 
-        var selector = $(this).attr('data-filter');
+    //     var selector = $(this).attr('data-filter');
 
-        $(".product-lists").isotope({
-            filter: selector,
-        });
+    //     $(".product-lists").isotope({
+    //         filter: selector,
+    //     });
         
-    });
+    // });
     
-    // isotop inner
-    $(".product-lists").isotope();
+    // // isotop inner
+    // $(".product-lists").isotope();
 
     // magnific popup
     $('.popup-youtube').magnificPopup({
