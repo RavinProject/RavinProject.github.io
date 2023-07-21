9beda20cdc981b293501be015802a77f4e36f26e
Regis Amaral <amaral.regis@gmail.com>
2023-07-20 21:55:17 -0300

    ajustes ainda não finalizados

---
+++ assets/js/ravin.js
@@ -0,0 +1,107 @@
+/** INIICIALIZADORES UNIVERSAIS*/
+(function ($) {
+    $(document).ready(function ($) {
+        atualizarTotal();
+        atualizarNumeroComanda();
+    });
+}(jQuery));
+/** FIM */
+
+function atualizarTotal() {
+    // Recuperar a comanda do localStorage
+    var comanda = JSON.parse(localStorage.getItem('comanda'));
+    var total = 0;
+
+    if(comanda !== null && comanda.itens !== null) {
+        // Calcular o total
+        for(var i = 0; i < comanda.itens.length; i++) {
+            total += comanda.itens[i].total;
+        }
+    }
+
+    // Atualizar o total na interface do usuário
+    document.getElementById("totalGasto").innerText = total.toFixed(2);
+}
+
+function atualizarNumeroComanda() {
+    // Recuperar a comanda do localStorage
+    var comanda = JSON.parse(localStorage.getItem('comanda'));
+
+    if(comanda !== null) {
+        // Atualizar o número da comanda na interface do usuário
+        document.getElementById("numero-comanda").innerText = comanda.numero;
+    }
+}
+
+// Caminho da imagem do modal precisa ser mudado mais tarde se bugar
+function preencherModal(item) {
+    var imageURL = "assets/img/products/" + item.imagem;
+    document.querySelector('#myModal .single-product-img img').src = imageURL;
+    document.querySelector('#myModal .single-product-content h3').innerText = item.nome;
+    document.querySelector('#myModal .single-product-content .single-product-pricing').innerText = 'R$' + item.valor;
+    document.querySelector('#myModal .single-product-content p:not(.single-product-pricing)').innerText = item.descritivo;
+    document.querySelector('#myModal #modal-categoria').innerText = item.categoria;
+}
+
+var comanda = {
+    "numero": 12222,
+    "itens": [
+        {
+            "item": { "nome": "Item 1", "valor": 10.0 },
+            "quantidade": 2,
+            "total": 2323230.0
+        },
+        {
+            "item": { "nome": "Item 2", "valor": 15.0 },
+            "quantidade": 3,
+            "total": 45.0
+        }
+    ],
+    "total": 65.0
+};
+
+localStorage.setItem('comanda', JSON.stringify(comanda));
+
+/**
+ * CARREGA OS ITENS PARA SELEÇÃO
+ */
+function carregarItens() {
+    var box_itens = document.getElementsByClassName('product-lists')[0];
+    fetch(`http://localhost:8080/`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                console.log('deu ruim!');
+                return;
+            }
+            let html = "";
+            for (let categoria in data) {
+                if (data.hasOwnProperty(categoria)) {
+                    data[categoria].forEach(item => {
+                        html += getBoxItem(categoria, item);
+                    });
+                }
+            }
+            box_itens.innerHTML = html;
+            // carrega os actions do template
+            loaderActions();
+            setTimeout(function () {
+                // isotop inner
+                $(".product-lists").isotope();
+            }, 500);
+        })
+        .catch(error => console.log(error));
+
+    function getBoxItem(categoria, item) {
+        return `<div class="col-lg-4 col-md-6 text-center ${categoria}">
+        <div class="single-product-item">
+            <div class="product-image">
+                <a href="#modalProdutoSelecionado" data-toggle="modal"><img src="assets/img/products/${item.imagem}" alt=""></a>
+            </div>
+            <h3>${item.nome}</h3>
+            <p class="product-price"><span>Un</span> R$ ${item.valor} </p>
+            <a href="#" class="cart-btn"><i class="fas fa-shopping-cart"></i> Adicionar a comanda</a>
+        </div>
+        </div>`;
+    }
+}
