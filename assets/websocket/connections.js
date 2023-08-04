d5aee8bb308de6499547d7527b3a6418a88a17a6
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-08-04 13:04:38 -0300

    reordenação de pastas

--- assets/websocket/connections.js
+++
@@ -1,5 +0,0 @@
-// Função para encontrar o índice do cliente na lista de clientes conectados
-exports.clientsConnected = [];
-exports.kitchenConnected = [];
-
-exports.getIndexByConnection = (connection) => exports.clientsConnected.findIndex((valor) => connection === valor);
